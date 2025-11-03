7e4b1ab0ee281ecfa3b424f53f1a3afd888a02b4
Emily-060503 <emilyquinonesgamiz1@gmail.com>
2025-11-03 02:13:06 -0600

    llave para base de datos

--- index.js
+++ index.js
@@ -16,41 +16,21 @@ app.use(express.static(path.join(__dirname, 'views'))); // Para acceder a styles
 
 // Ruta principal
 app.get('/', (req, res) => {
-    res.render('index');
+  res.render('index');
 });
 
 // Rutas para las diferentes categorías
-app.get('/colorido', (req, res) => {
-    res.render('colorido');
-});
-
-app.get('/gotico', (req, res) => {
-    res.render('gotico');
-});
-
-app.get('/hippie', (req, res) => {
-    res.render('hippie');
-});
-
-app.get('/minimalista', (req, res) => {
-    res.render('minimalista');
-});
-
-app.get('/nosotros', (req, res) => {
-    res.render('nosotros');
-});
-
-app.get('/contacto', (req, res) => {
-    res.render('contacto');
-});
+app.get('/colorido', (req, res) => res.render('colorido'));
+app.get('/gotico', (req, res) => res.render('gotico'));
+app.get('/hippie', (req, res) => res.render('hippie'));
+app.get('/minimalista', (req, res) => res.render('minimalista'));
+app.get('/nosotros', (req, res) => res.render('nosotros'));
+app.get('/contacto', (req, res) => res.render('contacto'));
 
 // Iniciar el servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en puerto ${PORT}`);
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
 
-powershell -Command "Start-Service ssh-agent"
-ssh-add %USERPROFILE%\.ssh\id_ed25519
-ssh-add -l
-git push -u origin main
+
